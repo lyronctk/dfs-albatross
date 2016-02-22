51deb76e24e515f87fb87f33591fb831aa17612d
Lyron <lyronctk@gmail.com>
2016-02-21 22:48:23 -0800

    Fix 500 error take 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  config.exceptions_app = self.routes
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
