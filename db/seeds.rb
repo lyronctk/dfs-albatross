bd774737e41ca442e5b4ad3d31b32ad87da0e6e0
lcotingkeh <lance@box.com>
2016-02-27 22:28:37 -0500

    Seed change + some small working and icon changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,41 +8,69 @@
 
 for i in 0..20
 	Golfer.create!(name: "Jordan Speith " + i.to_s,
-		          rank: 10, score_to_par: -10,
-		          last_update_hole: 17,
+		          rank: rand(1..150), score_to_par: rand(-9..13),
+		          last_update_hole: rand(3..18),
 		          last_update_round: 3, 
-		          probability_win: 0.13, 
-		          probability_make_cut: 0.51, 
-		          probability_mdf: 0.63, 
+		          probability_win: rand(0.01..0.20), 
+		          probability_make_cut: rand(0.20..0.85), 
+		          probability_mdf: rand(0.40..0.95), 
 		          contest_name: "Pebble Beach",
 		          stats: "Birdies+:2|Birdies:7|Par:19|Bogeys:6|Bogeys-:2")
 
-	GolfLineup.create!(rank: 23,
+	GolfLineup.create!(rank: rand(10..100000),
 					   contest_id: 1234,
 					   dk_name: "boozinbuffalo",
-					   dk_score: 240,
+					   dk_score: rand(150..420),
 					   golfer1: 6789,
 					   golfer2: 5445,
 					   golfer3: 5645,
 					   golfer4: 8877,
 					   golfer5: 2122,
 					   golfer6: 3435,
-					   probability_win: 0.02,
-					   probability_mc: 0.1,
-					   probability_mf: 0.15,
-					   expected_payout: 100)
-
-	if(i%2 == 0)
-		contest_name = 'Superbowl'
-	else
-		contest_name = 'World Cup'
-	end
-
-	Contest.create!(name: contest_name,
-					date_time: "2016-02-01 15:33:06",
-					entry_price: 3.50,
-					num_entries: 100000,
-					win: 0.04,
-					contest_type: "Cash",
-					expected_payoff: 50.5)
+					   probability_win: rand(0.001..0.10),
+					   probability_mc: rand(0.001..0.20),
+					   probability_mf: rand(0.001..0.30),
+					   expected_payout: rand(3..10000))
 end
+
+Contest.create!(name: "Birdie",
+				date_time: "2016-02-01 15:33:06",
+				entry_price: 3,
+				num_entries: 100000,
+				win: rand(0.01..0.05),
+				contest_type: "GPP",
+				expected_payoff: 50.5)
+
+
+Contest.create!(name: "Best Ball",
+				date_time: "2016-02-01 15:33:06",
+				entry_price: 27,
+				num_entries: 30000,
+				win: rand(0.01..0.05),
+				contest_type: "GPP",
+				expected_payoff: 140.5)
+
+
+Contest.create!(name: "Hybrid",
+				date_time: "2016-02-01 15:33:06",
+				entry_price: 3,
+				num_entries: 100000,
+				win: rand(0.01..0.05),
+				contest_type: "GPP",
+				expected_payoff: 140.5)
+
+Contest.create!(name: "Thunderdome",
+				date_time: "2016-02-01 15:33:06",
+				entry_price: 5300,
+				num_entries: 1000,
+				win: rand(0.01..0.10),
+				contest_type: "GPP",
+				expected_payoff: 7200)
+
+Contest.create!(name: "$5 Double Up",
+				date_time: "2016-02-01 15:33:06",
+				entry_price: 5,
+				num_entries: 50000,
+				win: rand(0.25..0.70),
+				contest_type: "Cash",
+				expected_payoff: 100)
